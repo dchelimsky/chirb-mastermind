e67c5b4766dc8b1b66e347d617d201f2c798f926
David Chelimsky <dchelimsky@gmail.com>
2009-03-02 19:35:24 -0600

    session 1 over

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -5,11 +5,24 @@ module Mastermind
     end
     
     def start(secret_code)
+      @secret_code = secret_code
       @messenger.puts "Welcome to Mastermind!"
       @messenger.puts "Enter guess:"
     end
     
     def guess(guess)
+      result = []
+      remainder = []
+      
+      guess.each_with_index do |color, index|
+        if color == @secret_code[index]
+          result << "b"
+        elsif @secret_code.include?(color)
+          result << "w"
+        end
+      end
+      puts result.sort.join("")
+      @messenger.puts result.sort.join("")
     end
   end
 end
--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -2,11 +2,11 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module Mastermind
   describe Game do
+    before(:each) do
+      @messenger = mock("messenger").as_null_object
+      @game = Game.new(@messenger)
+    end
     context "starting up" do
-      before(:each) do
-        @messenger = mock("messenger").as_null_object
-        @game = Game.new(@messenger)
-      end
 
       it "should send a welcome message" do
         @messenger.should_receive(:puts).with("Welcome to Mastermind!")
@@ -17,6 +17,39 @@ module Mastermind
         @messenger.should_receive(:puts).with("Enter guess:")
         @game.start(%w[a b c d])
       end
+    end
+    
+    context "marking a guess" do
+      context "with all four colors in the rights spots" do
+        it "should mark the guess bbbb" do
+          @messenger.should_receive(:puts).with("bbbb")
+          @game.start(%w[r g y c])
+          @game.guess(%w[r g y c])
+        end
+      end
+      context "with all four colors, two in the right spot" do
+        it "marks the guess bbww" do
+          @messenger.should_receive(:puts).with('bbww')
+          @game.start(%w[r g y c])
+          @game.guess(%w[r g c y])
+        end
+      end
+      context "with all four colors, only one in right spot" do
+        it "marks the guess bwww" do
+          @messenger.should_receive(:puts).with('bwww')
+          @game.start(%w[r g y c])
+          @game.guess(%w[y r g c])
+        end
+      end
+      
+      context "with two colors in the right spot, one of those in the wrong spot" do
+        it "marks the guess bb" do
+          @messenger.should_receive(:puts).with('bb')
+          @game.start(%w[r g y c])
+          @game.guess(%w[r g g b])
+        end
+      end
+      
     end
   end
 end
